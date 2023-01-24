6ace1def8f2d3938fab11c7064065cf2802906c6
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-24 15:23:47 +0300

    phase 1 fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    console.log(input.value);
+
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // LOG: (3) [{…}, {…}, {…}]
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
